1aff45e5594d847d4a0ef6a31f136df946c6fdd7
iver gentz <ivergentz@gmail.com>
2021-12-12 13:46:48 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Footer from './components/Foter'
 import Header from './components/Header'
+import Main from './components/Main'
 
 function App() {
   return (
     <div>
       <Header />
+      <Main />
       <Footer />
     </div>
   )
---
+++ src/components/Main.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Main = () => {
+  return <div>BODY</div>
+}
+
+export default Main
